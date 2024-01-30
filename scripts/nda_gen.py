d3ae5f78b2ccc6a2d6e26c5fd36aa096d8e83956
Daniel Asay <danielasay@fas.harvard.edu>
2024-01-30 12:04:55 -0500

    modify method of extracting key_file row

--- scripts/nda_gen.py
+++ scripts/nda_gen.py
@@ -60,11 +60,8 @@ def add_key_file_info(subjectkey, key_file):
     """
     Gather info from the key file for this file
     """
-    print(key_file)
 
-    sys.exit()
-
-    key_row = key_file.index[key_file['subjectkey'] == subjectkey[0]].tolist()[0]
+    key_row = key_file.index[key_file['subjectkey'] == value].tolist()[0]
 
     print(key_row)
 
